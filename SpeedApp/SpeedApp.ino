c57fc8b7f5e49d2c7e95faec36292c932cfc7713
Leif Westman <stallprobbe@gmail.com>
2016-04-24 12:29:52 +0200

    Add project files.

---
+++ SpeedApp/SpeedApp.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SpeedApp.ino
+ Created:	4/24/2016 12:29:49 PM
+ Author:	LW
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
